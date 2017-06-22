880eb107e9885357e2bb95bb3a25e8499eb06ea6
yairshlomi <sedjons@gmail.com>
2017-06-22 10:41:37 +0300

    some changes on comments

--- Pi/pi2_unit_1_sil.py
+++ Pi/pi2_unit_1_sil.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+
+#this script check if there is silenced sensor, and if so send the data to the sensor unit.
+
 from __future__ import division
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
@@ -8,6 +11,8 @@ import sys
 import requests
 import json
 
+
+#set the RaspberryPi's GPIO for comunnicating with sensor unit number 1 using the NRF24
 GPIO.setwarnings(False) 
 
 GPIO.setmode(GPIO.BCM)
@@ -31,6 +36,9 @@ radio.openReadingPipe(1, pipes[1])
 #radio.printDetails()
 # radio.startListening()
 
+
+#get the silenced sensors from the server, parse it and check if the query is not empty. 
+#if not empty, append the silences sensores_id to the message that will be sent to the sensor unit.
 r = requests.get('https://34.204.144.157/api.php?type=local_fd_get_silenced')
 array = r.json()
 data = json.loads(array)
@@ -42,11 +50,12 @@ if (len (tosil) < 2):
     sys.exit(0)
 
 
-message = list("tosil")
+message = list(tosil)
 
 while len(message) <  32:
     message.append(0)
 
+#send the message
 print ("Sending sample request to unit 1: ")
 def send_arduino_request():
     start = time.time()
@@ -77,13 +86,13 @@ def send_arduino_request():
     radio.stopListening()
     
     return string
-
+#try to send the data 30 times. if the sensor unit will get it, it will stop also
 print ("waiting for response")
 i = 0 
 while True:
     data1 = send_arduino_request()
         i+=1
-    if (i==30):
+    if (i==30 or len (data1) < 2):
 
         sys.exit()
 
